c851605eb3fa17e0b255003ff892881c53cfbcc2
Paul Ryan <paul@reillyryan.net>
2022-10-25 12:44:12 +1100

    masquerade traffic outbound from main interface to localise routing knowledge at the hub

--- templates/ansible-roles/wireguard_spoke/templates/post-up.sh
+++ templates/ansible-roles/wireguard_spoke/templates/post-up.sh
@@ -3,5 +3,5 @@
 iptables -A FORWARD -i wg0 -o {{ host_ifname }} -j ACCEPT
 iptables -A FORWARD -i {{ host_ifname }} -o wg0 -j ACCEPT
 
-iptables -t nat -A POSTROUTING -o wg0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o {{ host_ifname }} -j MASQUERADE
 
--- templates/ansible-roles/wireguard_spoke/templates/pre-down.sh
+++ templates/ansible-roles/wireguard_spoke/templates/pre-down.sh
@@ -3,5 +3,5 @@
 iptables -D FORWARD -i wg0 -o {{ host_ifname }} -j ACCEPT
 iptables -D FORWARD -i {{ host_ifname }} -o wg0 -j ACCEPT
 
-iptables -t nat -D POSTROUTING -o wg0 -j MASQUERADE
+iptables -t nat -D POSTROUTING -o {{ host_ifname }} -j MASQUERADE
 
